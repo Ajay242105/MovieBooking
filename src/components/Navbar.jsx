fdb4b369a677797b4f36d3eeaea379fc6060684d
ajay <ajay766613@gmail.com>
2025-07-03 00:35:01 +0545

    navbar page added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,36 @@
 import React from 'react'
-
+import { Link } from "react-router-dom";
+import { assets } from '../assets/assets';
+import {SearchIcon, XIcon} from 'lucide-react'
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='fixed top-0 left-0 z-50 w-full flex items-center justify-between px-6 md:px-16 py-5'>
+      {/* logo */}
+      <Link to='/' className='max-md:flex-1'>
+        <img src={assets.logo} alt='' className='w-36 h-auto' />
+      </Link>
+
+      {/* Nav button */}
+      <div className='max-md:absolute max-md:top-0 max-md:left-0 max-md:font-medium
+      max-md:text-lg z-50 flex flex-col md:flex-row items-center max-md:justify-center gap-8 py-3
+      max-md:h-screen min-md:rounded-full backdrop-blur bg-black/70 md:bg-white/10 md:border
+border-gray-300/20 overflow-hidden transition-[width] duration-300 px-6
+      '>
+        <XIcon className="md:hidden absolute top-6 right-6 w-6 h-6 cursor-pointer"/>
+        <Link to='/'>Home</Link>
+        <Link to='/movies'>Movies</Link>
+        <Link to='/'>Theaters</Link>
+        <Link to='/'>Releases</Link>
+        <Link to='/favorite'>Favorites</Link>
+
+      </div>
+      {/* search and login button */}
+      <div className='flex  items-center gap-8'>
+        <SearchIcon className="max-md:hidden w-6 h-6 cursor-pointer"/>
+        <button className='px-4 py-1 sm:px-7 sm:py-2 bg-primary hover:bg-primary-dull transition rounded-full font-medium cursor-pointer '>Login</button>
+
+      </div>
+    </div>
   )
 }
 
