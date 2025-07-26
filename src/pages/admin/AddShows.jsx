01a9780ce1dd7fa84d39eb2ab6889aafc68a6c11
ajay <ajay766613@gmail.com>
2025-07-27 00:43:07 +0545

    show added page aded in Admin

--- src/pages/admin/AddShows.jsx
+++ src/pages/admin/AddShows.jsx
@@ -8,24 +8,58 @@ import VoteCountFormat from '../../components/VoteCountFormat';
 const AddShows = () => {
   const currency = import.meta.env.VITE_CURRENCY || "₹";
   const [nowPlayingMovies, setNowPlayingMovies] = useState([]);
-  const [dataTimeSelection, setDataTimeSelection] = useState(null);
-const [dateTimeInput, setDateTimeInput] = useState("");
-const [showPrice, setShowPrice] = useState("");
-const [selectedMovie, setSelectedMovie] = useState(null);
+  const [dataTimeSelection, setDataTimeSelection] = useState({});
+  const [dateTimeInput, setDateTimeInput] = useState("");
+  const [showPrice, setShowPrice] = useState("");
+  const [selectedMovie, setSelectedMovie] = useState(null);
 
-const handleMovieSelect = (movieId) => {
-  setSelectedMovie(movieId);
-  setDataTimeSelection(movieId); // Optionally sync with select dropdown
-};
-const fetchNowPlayingMovies = async () => {
-  setNowPlayingMovies(dummyShowsData);
+  const handleMovieSelect = (movieId) => {
+    setSelectedMovie(movieId);
+    setDataTimeSelection(movieId); // Optionally sync with select dropdown
+  };
+  const fetchNowPlayingMovies = async () => {
+    setNowPlayingMovies(dummyShowsData);
 
 
-}
-useEffect(() => {
-  fetchNowPlayingMovies();
+  }
+  useEffect(() => {
+    fetchNowPlayingMovies();
 
-}, []);
+  }, []);
+  const handleDateTimeAdd = () => {
+    if (!dateTimeInput) {
+      return ;
+    }
+    const [date, time] = dateTimeInput.split('T');
+    if (!date || !time) {
+      return;
+    }
+    setDataTimeSelection(prev => {
+      const times=prev[date] || [];
+      if(!times.includes(time)){
+        return {
+          ...prev,
+          [date]: [...times, time]
+        };
+      }
+      return prev;
+    });
+  }
+
+  const handleRemoveTime = (date, time) => {
+    setDataTimeSelection(prev => {
+      const filteredTimes = prev[date].filter(t => t !== time);
+      if (filteredTimes.length === 0) {
+       
+        const { [date]: _, ...rest } = prev; // Remove date if no times left
+        return rest;
+      }
+      return {
+        ...prev,
+        [date]: filteredTimes,
+      };
+    });
+  }
   return nowPlayingMovies.length > 0 ? (
     <>
       <Title text1="Add" text2="Shows" />
@@ -52,19 +86,83 @@ useEffect(() => {
                   <p className='text-gray-300'><VoteCountFormat count={movie.vote_count} /> votes</p>
                 </div>
               </div>
-              {selectedMovie=== movie.id && (
+              {selectedMovie === movie.id && (
                 <div className='absolute top-2 right-2 flex items-center justify-center h-6 w-6 rounded-full bg-primary'>
-                  <CheckIcon className='w-6 h-6' />
+                  <CheckIcon className='w-6 h-6' strokeWidth={2.5} />
                 </div>
               )}
+              <p className=' text-md text-gray-300 font-bold truncate'>{movie.title}</p>
+              <p className=' text-sm text-gray-500'>{movie.tagline}</p>
+              <p className=' text-sm text-gray-500'>{movie.release_date}</p>
             </div>
           ))}
         </div>
       </div>
-      <div className='max-w-4xl mt-6 bg-black'>
-        <form className='space-y-4'>
-          <div>
-            <label className='block text-sm font-medium mb-1'>Select Movie</label>
+      <div className='mt-6 '>
+
+        <label className='block text-sm font-medium mb-2'>Show Price</label>
+        <div className='inline-flex items-center gap-2 border border-gray-300 rounded-md mb-2'>
+          <p className='text-sm text-gray-400 ml-3'>{currency}</p>
+          <input
+            min={0}
+            type="number"
+            value={showPrice}
+            onChange={(e) => setShowPrice(e.target.value)}
+            placeholder='Enter show price'
+            className='outline-none p-2 '
+          />
+        </div>
+
+      </div>
+      {/* Date and Time Selection */}
+      <div>
+        <label className='block text-sm font-medium mb-2'>
+          Show Date & Time</label>
+        <div className='inline-flex items-center gap-2 border border-gray-300 rounded-md mb-2'>
+          <input
+            type="datetime-local"
+            value={dateTimeInput}
+            onChange={(e) => setDateTimeInput(e.target.value)}
+            className='outline-none p-2 '
+          />
+          <button onClick={handleDateTimeAdd} className='bg-primary/80 text-white px-3 py-3 text-sm rounded-md hover:bg-primary cursor-pointer'>
+            Add Time
+          </button>
+        </div>
+
+      </div>
+      {/* display selected date and time */}
+      <div className='mt-4'>
+        {Object.keys(dataTimeSelection).length > 0 && (
+          <div className='mt-6'>
+            <h2 className='text-lg font-medium mb-2'>Selected Date & Times</h2>
+            <ul>
+              {Object.entries(dataTimeSelection).map(([date, times]) => (
+                <li key={date} className='mb-2'>
+                  <div className='font-medium'>{date}</div>
+                  <div className="flex flex-wrap gap-2 mt-1 text-sm">
+                    {times.map((time, index) => (
+                      <div key={index} className='flex items-center gap-2 bg-primary/10 border border-primary/20 rounded-md px-3 py-1'>
+                        <span>{new Date(time).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}</span>
+                        <button
+                          onClick={() => handleRemoveTime(date, time)}
+                          className='text-red-500 hover:text-red-700'
+                        >
+                          Remove
+                        </button>
+                      </div>
+                    ))}
+                  </div>
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
+      </div>
+
+      <form className='space-y-4'>
+        <div>
+          <label className='block text-sm font-medium mb-1'>Select Movie</label>
           <select
             className='w-full p-2 border  rounded bg-primary/10 border-primary'
             onChange={(e) => setDataTimeSelection(e.target.value)}
@@ -75,24 +173,8 @@ useEffect(() => {
             ))}
           </select>
         </div>
-        <div>
-          <label className='block text-sm font-medium mb-1'>Show Date & Time</label>
-          <input
-            type="datetime-local"
-            value={dateTimeInput}
-            onChange={(e) => setDateTimeInput(e.target.value)}
-            className='w-full p-2 border border-gray-300 rounded'
-          />
-        </div>
-        <div>
-          <label className='block text-sm font-medium mb-1'>Show Price</label>
-          <input
-            type="number"
-            value={showPrice}
-            onChange={(e) => setShowPrice(e.target.value)}
-            className='w-full p-2 border border-gray-300 rounded'
-          />
-        </div>
+
+
         <button
           type="submit"
           className='px-4 py-2 bg-primary text-white rounded hover:bg-primary/80'
@@ -100,7 +182,6 @@ useEffect(() => {
           Add Show
         </button>
       </form>
-    </div>
     </>
   ) : (
     <Loading />
